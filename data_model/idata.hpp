3d1cbbf5321a4792ad5ff3286015420cd8cf3b88
Artemenko Anton <muroshechka@gmail.com>
2026-05-28 12:20:00 +0700

    add data stuct

---
+++ data_model/idata.hpp
@@ -0,0 +1,18 @@
+/// @file idata.hpp
+/// @brief Интерфейс данных
+/// @author Artemenko Anton
+#ifndef GUID_47aad79f_00cb_4717_8578_185d1fa9915b
+#define GUID_47aad79f_00cb_4717_8578_185d1fa9915b
+
+namespace data
+{
+class IData
+{
+  public:
+    virtual ~IData() = default;
+
+    /// @brief Метод для получения данных.
+    virtual void GetData() = 0;
+};
+} // namespace data
+#endif // GUID_47aad79f_00cb_4717_8578_185d1fa9915b
---
+++ data_model/src/timeline_data.cpp
@@ -0,0 +1,6 @@
+/// @file timeline_data.cpp
+/// @brief Определение класса TimelineData
+/// @author Artemenko Anton
+
+#include <data_model/src/timeline_data.hpp>
+using data::TimelineData;
